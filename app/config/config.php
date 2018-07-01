fe5ee1401c4b8136fb191e79cd3f318478de532f
Chris Boyd <cboyd2011@aol.com>
2018-07-01 16:21:32 -0400

    master rollback

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Boyd
+ * Date: 6/30/2018
+ * Time: 9:31 PM
+ */
+
+// Template Config
+define('__HEADER__', dirname(dirname(__FILE__)) . '/views/header.php');
+define('__FOOTER__', dirname(dirname(__FILE__)) . '/views/footer.php');
+
+// Database Config
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'bible app');
